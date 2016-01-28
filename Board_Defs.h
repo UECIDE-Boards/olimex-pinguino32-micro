f7d62ae6e708289785857633b0354d1420ea6ed4
Matt Jenkins <matt@majenko.co.uk>
2016-01-28 10:21:29 +0000

    Fixed redefinition warnings

--- Board_Defs.h
+++ Board_Defs.h
@@ -284,6 +284,7 @@ const static uint8_t SCK  = CONU_9;		// PIC32 SCK2
 #define digitalPinToTimerIC(P)  ( (digital_pin_to_timer_PGM[P] & _MSK_TIMER_IC)  )
 #define digitalPinToTimerTCK(P) ( (digital_pin_to_timer_PGM[P] & _MSK_TIMER_TCK) )
 #define	digitalPinToTimer(P)	digitalPinToTimerOC(P)
+#undef portRegisters
 #define portRegisters(P) ( port_to_tris_PGM[P] )
 #define portModeRegister(P) ( (volatile uint32_t *)port_to_tris_PGM[P] )
 #define portInputRegister(P) ( (volatile uint32_t *)(port_to_tris_PGM[P] + 0x0010) )
@@ -293,12 +294,14 @@ const static uint8_t SCK  = CONU_9;		// PIC32 SCK2
 //#define	digitalPinToAnalog(P) ( (((P) > 15) && ((P) < 32)) ? (P)-16 : NOT_ANALOG_PIN )
 // This definition can be used for the non-default case where there
 // is a mapping table to go from digital pin to analog pin
+#undef digitalPinToAnalog
 #define	digitalPinToAnalog(P) ( digital_pin_to_analog_PGM[P] )
 
 // This definition can be used for the default one-to-one mapping
 //#define analogInPinToChannel(P) ( P )
 // This definition is used when there isn't a one-to-one mapping
 // This uses a table to do the mapping.
+#undef analogInPinToChannel
 #define analogInPinToChannel(P) ( analog_pin_to_channel_PGM[P]  )
 
 /* ------------------------------------------------------------ */
